c28057d3316882e47b445ff479075922c8b00902
Vito <vslavilla@gmail.com>
2017-05-08 09:22:53 -0400

    wip

---
+++ tests/index.spec.ts
@@ -0,0 +1,12 @@
+// import * as expect from 'expect';
+import test from 'ava';
+
+test('it does', t => {
+  // expect(true).toBe(true);
+  t.is(true, true);
+});
+
+test('foo', t => {
+  t.is(false, false);
+});
+
